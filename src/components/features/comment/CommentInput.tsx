767e0043872999a412372b668fc43b67327f1a00
twy8939 <twy8939@naver.com>
2025-01-31 16:53:50 +0900

    feat: update CommentInput component

--- src/components/features/comment/CommentInput.tsx
+++ src/components/features/comment/CommentInput.tsx
@@ -1,14 +1,22 @@
-import { Flex, Stack } from "@atlaskit/primitives";
-import Textfield from "@atlaskit/textfield";
+import { Box, Flex, Stack, xcss } from "@atlaskit/primitives";
 import React, { useState } from "react";
 import Button from "@atlaskit/button/new";
 import { useTranslation } from "react-i18next";
+import TextArea from "@atlaskit/textarea";
+
+const commentInputStyles = xcss({
+  border: "1px solid #ddd",
+  borderRadius: "8px",
+  fontSize: "14px",
+  width: "100%",
+  padding: "space.100",
+});
 
 const CommentInput = () => {
   const { t } = useTranslation("common");
   const [comment, setComment] = useState("");
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === "Enter" && comment.trim()) {
       handleAddComment();
     }
@@ -18,15 +26,18 @@ const CommentInput = () => {
     console.log(comment);
   };
   return (
-    <Stack space="space.200">
-      <Textfield
-        placeholder={t("comment_placeholder")}
-        value={comment}
-        onKeyDown={handleKeyPress}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setComment(e.target.value)
-        }
-      />
+    <Stack space="space.100">
+      <Box xcss={commentInputStyles}>
+        <TextArea
+          appearance="none"
+          placeholder={t("comment_placeholder")}
+          value={comment}
+          onKeyDown={handleKeyPress}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+            setComment(e.target.value)
+          }
+        />
+      </Box>
       <Flex justifyContent="end">
         <Button
           appearance="primary"
